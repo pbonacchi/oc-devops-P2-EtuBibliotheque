5f14bc5032a94320be7f1f0afbf3d67028732633
Patrick Bonacchi <patrick.bonacchi@gmail.com>
2026-05-27 14:29:33 +0200

    CI basique (UT+E2E)

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://localhost:4200',
+    baseUrl: 'http://127.0.0.1:4200',
   },
 });
